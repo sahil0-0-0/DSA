b7e68f9a0f4f11777a5422de7ed6432555425d80
sahil <123296616+sahilafre@users.noreply.github.com>
2024-01-14 18:52:52 +0530

    Add files via upload

---
+++ 4. Loop/Demo.java
@@ -0,0 +1,9 @@
+//Print 1 to 10 number using For loop
+
+public class Demo {
+    public static void main ( String[] args ) {
+        for ( int i = 1 ; i <= 10 ; i++ ) {
+            System.out.println(i);
+        }
+    }
+}
