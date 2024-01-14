b5c3cbce2929cf94b0d1cc68fc48ef346db06a11
sahil <123296616+sahilafre@users.noreply.github.com>
2024-01-14 18:51:54 +0530

    Add files via upload

---
+++ 4. Loop/Demo.java
@@ -0,0 +1,11 @@
+//Print 1 to 10 number using While loop
+
+public class Demo {
+    public static void main ( String[] args ) {
+        int n = 1 ;
+        while ( n <= 10 ) {
+            System.out.println(n) ;
+            n++ ;
+        }
+    }
+}
