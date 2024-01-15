85f9722ac57097492356bb0535448ffae1ca2eca
sahil <123296616+sahilafre@users.noreply.github.com>
2024-01-16 01:10:58 +0530

    Add files via upload

---
+++ 5. Array/Demo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner ;
+public class Demo {
+    public static void main ( String[] args ) {
+        Scanner n = new Scanner( System.in ) ;
+        System.out.print("Enter size of the Array : ") ;
+        int size = n.nextInt() ;
+        int[] ar = new int[size] ;
+        System.out.println("\nEnter Array elements : ");
+        for ( int i=0 ; i<size ; i++ ) {
+            ar[i] = n.nextInt() ;
+        }
+        System.out.println("Array elemets : " ) ;
+        for ( int ele : ar ) {
+            System.out.println(ele) ;
+        }
+    }
+}
