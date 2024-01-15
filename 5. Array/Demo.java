5e7381a89bb4b86356468a3742fcc05c747834a1
sahil <123296616+sahilafre@users.noreply.github.com>
2024-01-16 01:27:31 +0530

    Add files via upload

---
+++ 5. Array/Demo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner ;
+public class Demo {
+    public static void main ( String[] args ) {
+        Scanner n = new Scanner( System.in ) ;
+        System.out.print("Enter size of the Array : ") ;
+        int size = n.nextInt() ;
+        int[] ar = new int[size] ;
+        System.out.println("\nEnter Array elements : ");
+        for ( int i=0 ; i<size ; i++ ) {
+            ar[i] = n.nextInt() ;
+        }
+        System.out.println("Odd number from the Array : " ) ;
+        for ( int i=0 ; i<size ; i++ ) {
+            if ( ar[i] % 2 != 0 ) {
+                System.out.print(ar[i] + " ");
+            }
+        }
+    }
+}
